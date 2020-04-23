badbe53dc0f1baee34154c8b3de1f87b2162b114
lennartdeknikker <me@lenn4rt.com>
2020-04-23 15:10:55 +0200

    transfer data to dom

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -11,36 +11,9 @@ function triggerUpload() {
 
 uploadButton.addEventListener('change', onFileChange, false)
 
-
-
 function onFileChange() {
   button.classList.add('hidden')
   text.classList.remove('red', 'hidden')
   text.innerText = 'Gelukt!'
-
-  convertXlsToJsonAndThen(event.target.files[0], handleJson)  
-}
-
-function convertXlsToJsonAndThen(file, callback) {
-  const fileReader = new FileReader()
-  fileReader.result = []
-
-  fileReader.onload = function(event) {
-    const data = event.target.result
-    const workbook = XLSX.read(data, {
-      type: 'binary'
-    })
-
-    workbook.SheetNames.forEach(sheet => {
-      let rowObject = XLSX.utils.sheet_to_row_object_array(
-        workbook.Sheets[sheet]
-      )
-      callback(JSON.stringify(rowObject))
-    })
-  }
-  fileReader.readAsBinaryString(file)
-}
-
-function handleJson(jsonData) {
-  console.log(jsonData)    
+  convertXlsToJsonAndThen(event.target.files[0], handleData)  
 }
