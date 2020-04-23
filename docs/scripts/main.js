f381fbe37ae071d1368ab0bb0cdc19112740ef89
lennartdeknikker <me@lenn4rt.com>
2020-04-23 13:10:48 +0200

    create file upload interface

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,10 +1,27 @@
-console.log(geoJson);
 
-async function loadMap(geoJson) {
-d3.json(geoJson).then(mapData => renderMap(mapData))
+const button = document.querySelector("#upload-button")
+const uploadButton = document.querySelector(".file-upload")
+const text = document.querySelector("h1")
+
+button.addEventListener("click", triggerUpload)
+
+function triggerUpload() {
+  uploadButton.click()
 }
 
-function renderMap() {}
+uploadButton.addEventListener("change", onFileChange, false)
 
-
-loadMap(europeJson)
+function onFileChange() {
+  if (!uploadButton.files[0].name.slice(-5).includes(".xls")) {
+    text.innerText = "Sorry, dat is geen excel-bestand."
+    text.classList.add("red")
+    button.innerText = "PROBEER OPNIEUW"
+    button.classList.remove("hidden")
+  } else {
+    button.classList.add("hidden")
+    text.classList.remove("red")
+    text.innerText = "Gelukt!"
+  }
+  text.classList.remove("hidden")
+  console.log(uploadButton.files[0].name)
+}
