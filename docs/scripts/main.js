46be0599c3a65b1b05fb44930a4b90989fe0ea05
lennartdeknikker <me@lenn4rt.com>
2020-04-23 14:16:06 +0200

    convert excel to json

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,34 +1,46 @@
 
-const button = document.querySelector("#upload-button")
-const uploadButton = document.querySelector(".file-upload")
-const text = document.querySelector("h1")
+const button = document.querySelector('#upload-button')
+const uploadButton = document.querySelector('.file-upload')
+const text = document.querySelector('h1')
 
-button.addEventListener("click", triggerUpload)
+button.addEventListener('click', triggerUpload)
 
 function triggerUpload() {
   uploadButton.click()
 }
 
-uploadButton.addEventListener("change", onFileChange, false)
+uploadButton.addEventListener('change', onFileChange, false)
+
+
 
 function onFileChange() {
-  if (!uploadButton.files[0].name.slice(-5).includes(".xls")) {
-    text.innerText = "Sorry, dat is geen excel-bestand."
-    text.classList.add("red")
-    button.innerText = "PROBEER OPNIEUW"
-    button.classList.remove("hidden")
-  } else {
-    button.classList.add("hidden")
-    text.classList.remove("red")
-    text.innerText = "Gelukt!"
-    manipulateExcelFile(uploadButton.files[0])
-  }
-  text.classList.remove("hidden")
-  console.log('file changed to', uploadButton.files[0].name);
-  
+  button.classList.add('hidden')
+  text.classList.remove('red', 'hidden')
+  text.innerText = 'Gelukt!'
+
+  convertXlsToJsonAndThen(event.target.files[0], handleJson)  
 }
 
-function manipulateExcelFile(file) {
-  console.log(file);
-  
+function convertXlsToJsonAndThen(file, callback) {
+  const fileReader = new FileReader()
+  fileReader.result = []
+
+  fileReader.onload = function(event) {
+    const data = event.target.result
+    const workbook = XLSX.read(data, {
+      type: 'binary'
+    })
+
+    workbook.SheetNames.forEach(sheet => {
+      let rowObject = XLSX.utils.sheet_to_row_object_array(
+        workbook.Sheets[sheet]
+      )
+      callback(JSON.stringify(rowObject))
+    })
+  }
+  fileReader.readAsBinaryString(file)
+}
+
+function handleJson(jsonData) {
+  console.log(jsonData)    
 }
